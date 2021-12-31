8726d4117a2fbe4f5de04fe956047037c79fd47e
BastianCorlay <96916556+BastianCorlay@users.noreply.github.com>
2021-12-31 15:28:19 +0100

    Create arbre.h

---
+++ arbre.h
@@ -0,0 +1,19 @@
+TArbre NouvelArbreVide( void );
+
+void Ajouter( TArbre* arbre , int val, char nom );
+
+int chercher(TArbre arbre,int v);
+  
+ int chercher2(TArbre arbre,char n);
+
+  void AfficherValeur( TArbre arbre );
+  
+  
+  void AfficherNom( TArbre arbre );
+  
+  
+  int NombreElements( TArbre arbre );
+  
+  int Hauteur( TArbre arbre );
+  
+  int delElement(TArbre* arbre, int info);
